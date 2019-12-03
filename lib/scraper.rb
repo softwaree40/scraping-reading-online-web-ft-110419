7465a09a126a9f2cd7e8824b8992fee11310f163
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-03 07:38:57 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,8 @@
+require 'pry'
 require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+list_link_text = doc.css("li > a")
+list_link_text.each { |t| puts t.text }
