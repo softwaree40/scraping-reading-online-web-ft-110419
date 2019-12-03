ac71ac517d14d7f0a708f73e4981aa351f1e6ed6
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-12-03 21:53:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,5 +4,9 @@ require 'open-uri'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
+<<<<<<< HEAD
 list_link_text = doc.css("li > a")
 list_link_text.each { |t| puts t.text }
+=======
+pages = doc.css("")
+>>>>>>> 481a5fa0a5b44f06a913e7b9462c272ebe4787c2
